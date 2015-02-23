d5f8793fb05196c1441e3ca4d142ffe2d881828e
Mauricio <maurimanuel92@hotmail.es>
2015-02-23 17:33:03 +0000

    Empezando con karma

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp    = require('gulp'),
 var del     = require('del');
 var minifyHTML = require('gulp-minify-html');
 var minifyCSS  = require('gulp-minify-css');
+var karma   = require('gulp-karma');
 
 gulp.task('minify', function () {
   gulp.src('temperature.js')
@@ -22,4 +23,25 @@ gulp.task('minify', function () {
 
 gulp.task('clean', function(cb) {
   del(['minified/*'], cb);
+});
+
+gulp.task('test', function() {
+  // Be sure to return the stream
+  return gulp.src([])
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      throw err;
+    });
+});
+
+gulp.task('default', function() {
+  gulp.src([])
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'watch'
+    }));
 });
