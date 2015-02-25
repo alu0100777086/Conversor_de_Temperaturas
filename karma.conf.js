f8d93cd662175cd5933a1b238b1f372fa2f39c49
Daniel E. Quijada Diaz <daniventf@gmail.com>
2015-02-25 20:15:59 +0000

    Añadido PhantomJS a Karma

--- karma.conf.js
+++ karma.conf.js
@@ -91,7 +91,7 @@ module.exports = function(config) {
   };
 
   if(process.env.TRAVIS){
-    configuration.browsers = ['Firefox', 'Chrome_travis_ci'];
+    configuration.browsers = ['PhantomJS', 'Firefox', 'Chrome_travis_ci'];
    }
 
    config.set(configuration);
